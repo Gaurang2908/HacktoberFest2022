13db4c61ea2c270ae01e3393ad5a56634751931f
sanidhya290105 <116171679+sanidhya290105@users.noreply.github.com>
2022-10-19 17:48:29 +0530

    Create diff.cpp
    The Difference Between int main( ), void main( ) and int main (void)

---
+++ diff.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h> //headerfiles
+
+int main()
+
+{
+
+printf(” Hello World!\n“);
+
+return 0;  //returning 0 of data type int 
+
+}
+
+ 
+
+#include<iostream.h> //headerfile
+
+int main()
+
+{
+
+cout<<” Hello World!\n“;
+
+return 0;  //returning 0 of data type int 
+
+}
