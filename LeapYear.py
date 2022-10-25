6ccc0a6f57756907ec799eed3ffdefa8fd3397f3
gummithaanushka <101972595+gummitha-anushka@users.noreply.github.com>
2022-10-25 18:36:05 +0530

    Update LeapYear.py
    This is a simplified code for finding whether a year is leap or not.

--- LeapYear.py
+++ LeapYear.py
@@ -1,9 +1,5 @@
 year=int(input("Enter the year:"))
-if (year % 400 == 0) and (year % 100 == 0):
-    print("{0} is a leap year".format(year))
-
-elif (year % 4 ==0) and (year % 100 != 0):
-    print("{0} is a leap year".format(year))
-
+if (year % 400 == 0 or (year%4==0 and year%100!=0)):  #For a year to be leap year, it is necessary that it is divisible by 4 and 400 but not by 100 only.
+    print(year,"is a leap year.")
 else:
-    print("{0} is not a leap year".format(year))
+    print(year,"is not a leap year.")
