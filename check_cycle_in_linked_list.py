243532730b6ca2a38a8c522fb0f1437ad85d9f1a
JosephDM-17 <josephdominicmathew@gmail.com>
2022-10-25 20:33:35 +0530

    Added a program to detect cycle in a linked list

---
+++ check_cycle_in_linked_list.py
@@ -0,0 +1,61 @@
+# Python3 program to detect cycle in the linked list
+ 
+
+ 
+ 
+class Node:
+ 
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+ 
+ 
+class LinkedList:
+ 
+    #  initialize head
+    def __init__(self):
+        self.head = None
+ 
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+ 
+
+    def printList(self):
+        temp = self.head
+        while(temp):
+            print(temp.data, end=" ")
+            temp = temp.next
+ 
+    def detectLoop(self):
+        s = set()
+        temp = self.head
+        while (temp):
+ 
+          
+            if (temp in s):
+                return True
+ 
+          
+            s.add(temp)
+ 
+            temp = temp.next
+ 
+        return False
+ 
+ 
+llist = LinkedList()
+llist.push(20)
+llist.push(4)
+llist.push(15)
+llist.push(10)
+ 
+# Create a loop for testing
+llist.head.next.next.next.next = llist.head
+ 
+if(llist.detectLoop()):
+    print("Loop found")
+else:
+    print("No Loop ")
