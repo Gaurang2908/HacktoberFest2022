89ffbff7f5d240f55142009acbd8892876a0d45c
Lakshya Tyagi <tyagicoolboy22@gmail.com>
2022-10-28 01:01:59 +0530

    Create numberOnlyValidation.js

---
+++ numberOnlyValidation.js
@@ -0,0 +1,7 @@
+public numberOnlyValidation(event: any) {
+    const pattern = /[0-9]/;
+    let inputChar = String.fromCharCode(event.charCode);
+    if (!pattern.test(inputChar)) {
+        event.preventDefault();
+    }
+}
