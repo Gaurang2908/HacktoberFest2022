4cae8a6359b299c628fc31ea07b75408dd523579
Virendra_kumar <117063481+VRNDRYDV@users.noreply.github.com>
2022-10-31 12:13:57 +0530

    Create even.cpp

---
+++ even.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cout<<"enter a number:";
+cin>> n;
+if(n%2==0){
+cout<<"number is even"<<n<<endl;
+}
+else{
+cout<<"number is odd";
+}
+return 0;
+}
